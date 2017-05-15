a8f319926ff2ec920e35277787365561d3c98c0c
leovt <leonhard.vogt@gmx.ch>
2017-05-16 00:10:22 +0200

    merged with command.py

--- mzdisasm.py
+++ mzdisasm.py
@@ -3,8 +3,8 @@ import capstone
 import os
 import sys
 import json
+import cmd
 from collections import namedtuple, defaultdict
-from cmd import Cmd
 import binascii
 
 MZHEADER = '<2s13H'
@@ -14,33 +14,66 @@ tMZHEADER = namedtuple('MZHEADER', 'ID sz_last nb_pages nb_reloc sz_header '
 md = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_16)
 md.details = True
 
-class Command(Cmd):
+class Command(cmd.Cmd):
     prompt = '(mzdisasm) '
 
+    def __init__(self, state):
+        cmd.Cmd.__init__(self)
+        self.state = state
+
     def do_save(self, arg):
-        self.info.save()
+        self.state.save()
 
     def do_header(self, arg):
-        print(self.info.header)
+        print(self.state.header)
 
     def do_quit(self, arg):
-        self.info.save()
+        '''quit
+           save the session and quit the application'''
+        self.state.save()
         return True
 
     def do_goto(self, arg):
-        adr = int(arg, 0)
-        self.current = adr
-        self.disasm = iter(md.disasm(self.info.data[adr:], adr))
-        for i, instr in enumerate(self.disasm):
-            for lbl in self.info.labels.get(instr.address, []):
-                print('%s:' % lbl)
-            print("%s 0x%x: %-10s  %-6s  %s" % ('->' if instr.address == self.current else '  ',
-                                             instr.address,
-                                             binascii.hexlify(instr.bytes).decode('ascii'),
-                                             instr.mnemonic,
-                                             instr.op_str))
-            if i>5:
+        '''goto [address]
+           set the current address to [address]'''
+        try:
+            address = int(arg, 0)
+        except ValueError:
+            print('Illegal argument %r' % arg)
+            return
+        self.state.goto(address)
+        print(self.state.show())
+
+
+    def do_show(self, arg):
+        '''show [!]lines
+           display the given number of lines of data.
+           by default the next lines are shown.
+           if the form with ! is used, the lines are
+           shown starting from the current position.'''
+        if arg[0] == '!':
+            self.state.reset_shown()
+            arg = arg[1:]
+        try:
+            lines = int(arg, 0)
+        except ValueError:
+            print('Illegal argument %r' % arg)
+            return
+        for i in range(lines):
+            line = self.state.show()
+            print(line)
+            if line == 'EOF':
                 break
+
+    def do_mode(self, arg):
+        arg = arg.lower()
+        if arg not in ('hex', 'bin', 'asm'):
+            print('Illegal argument %r' % arg)
+            return
+        self.state.set_displaymode(arg)
+
+    def emptyline(self):
+        print(self.state.show())
 
     def do_label(self, arg):
         arg = arg.split()
@@ -48,10 +81,11 @@ class Command(Cmd):
             adr = self.current
         else:
             adr = int(arg[1], 0)
-        self.info.labels.setdefault(adr, [])
-        self.info.labels[adr].append(arg[0])
+        self.state.labels.setdefault(adr, [])
+        self.state.labels[adr].append(arg[0])
 
-class ExeInfo:
+
+class State(object):
     def __init__(self, fname):
         self.fname = fname
         with open(fname, 'rb') as f:
@@ -64,15 +98,54 @@ class ExeInfo:
             with open(fname+'.json', 'r') as f:
                 j = json.load(f)
                 self.labels = {int(k):v for k,v in j['labels'].items()}
+        self.address = 0
+        self.mode = 'hex'
+        self.reset_shown()
+
+    def show(self):
+        return next(self.lines, 'EOF')
+
+    def goto(self, address):
+        self.address = address
+        self.reset_shown()
+
+    def set_displaymode(self, mode):
+        self.mode = mode
+        self.reset_shown()
+
+    def reset_shown(self):
+        if self.mode == 'hex':
+            self.lines = (
+                '%2s 0x%06X: %s  %s  %s' % (
+                    '->' if self.address == a else '', a,
+                    ' '.join('%02X' % c for c in self.data[a:a+8]),
+                    ' '.join('%02X' % c for c in self.data[a+8:a+16]),
+                    ''.join((chr(c) if 32 <= c < 127 else '.') for c in self.data[a:a+16]))
+                for a in range(self.address, len(self.data), 16))
+        elif self.mode == 'bin':
+            self.lines = (
+                '%2s 0x%06X:  %s  (0x%02X)  %s' % (
+                    '->' if self.address == a else '', a,
+                    bin(c)[2:].rjust(8,'0'), c, (chr(c) if 32 <= c < 127 else ''))
+                for (a,c) in enumerate(self.data[self.address:], self.address))
+        elif self.mode == 'asm':
+            def lines():
+                for instr in md.disasm(self.data[self.address:], self.address):
+                    for lbl in self.labels.get(instr.address, []):
+                        yield '%s:' % lbl
+                    yield ("%s 0x%x: %-10s  %-6s  %s" % (
+                            '->' if instr.address == self.address else '  ',
+                            instr.address,
+                            binascii.hexlify(instr.bytes).decode('ascii'),
+                            instr.mnemonic, instr.op_str))
+            self.lines = iter(lines())
+
 
     def save(self):
         with open(self.fname+'.json', 'w') as f:
             j = json.dump({'labels':self.labels}, f)
 
-if __name__ == '__main__':
-    cmd = Command()
-    cmd.info = ExeInfo(sys.argv[1])
-    cmd.current = 0
-    cmd.cmdloop()
 
-path = os.path.expanduser('~/Games/dosbox/coloniz2/')
+if __name__ == '__main__':
+    cmd = Command(State(sys.argv[1]))
+    cmd.cmdloop()
